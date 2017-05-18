1346450874f155d6674dd62d5dae88db265db54b
Hauke Stange <hauke.stange@jimdo.com>
2017-05-18 13:06:08 +0200

    Add Hello container and connect store

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import Hello from './components/Hello';
+import { enthusiasm } from './reducers';
+import { StoreState } from './types';
+
+import Hello from './containers/Hello';
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
