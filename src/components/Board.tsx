5857913015904d7b1b8aa3ef16610aced74b8917
Hauke Stange <hauke.stange@jimdo.com>
2017-05-18 17:33:42 +0200

    Move state from Game to Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,42 +1,22 @@
 import * as React from 'react';
 
-import { EMPTY_FIELD, PLAYER_ONE, PLAYER_TWO } from '../constants';
 import { Field } from '../types';
 
 import Square from './Square';
 
 import './Board.css';
 
-export interface Props { };
-
-export interface State {
+export interface Props {
   squares: Field[];
-  isPlayerOneNext: boolean;
+  onClick: (i: number) => void;
 };
 
+export interface State { };
+
 class Board extends React.Component<Props, State> {
-  public constructor() {
-    super();
-    this.state = {
-      squares: Array<Field>(9).fill(EMPTY_FIELD),
-      isPlayerOneNext: true,
-    };
-  }
-
   public render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status: string;
-
-    if (winner !== EMPTY_FIELD) {
-      status = `Winner: ${winner}`;
-    } else {
-      const nextPlayer = this.state.isPlayerOneNext ? PLAYER_ONE : PLAYER_TWO;
-      status = `Next player ${nextPlayer}`;
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(1)}
           {this.renderSquare(2)}
@@ -56,46 +36,13 @@ class Board extends React.Component<Props, State> {
     );
   }
 
-  private handleClick(i: number) {
-    const squares = this.state.squares.slice();
-    if (this.calculateWinner(squares) !== EMPTY_FIELD || squares[i] !== EMPTY_FIELD) {
-      return;
-    }
-    squares[i] = this.state.isPlayerOneNext ? PLAYER_ONE : PLAYER_TWO;
-    this.setState({
-      squares,
-      isPlayerOneNext: !this.state.isPlayerOneNext
-    });
-  }
-
   private renderSquare(i: number) {
     return (
       <Square
-        field={this.state.squares[i - 1]}
-        onClick={() => this.handleClick(i - 1)}
+        field={this.props.squares[i - 1]}
+        onClick={() => this.props.onClick(i - 1)}
       />
     );
-  }
-
-  private calculateWinner(squares: Field[]): Field {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return EMPTY_FIELD;
   }
 }
 
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { EMPTY_FIELD } from '../constants';
+import { EMPTY_FIELD, PLAYER_ONE, PLAYER_TWO } from '../constants';
 import { Field, History } from '../types';
 
 import Board from './Board';
@@ -26,17 +26,71 @@ class Game extends React.Component<Props, State> {
   }
 
   public render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = this.calculateWinner(current.squares);
+
+    let status: string;
+
+    if (winner !== EMPTY_FIELD) {
+      status = `Winner: ${winner}`;
+    } else {
+      const nextPlayer = this.state.isPlayerOneNext ? PLAYER_ONE : PLAYER_TWO;
+      status = `Next player ${nextPlayer}`;
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
+  }
+
+  private handleClick(i: number) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (this.calculateWinner(squares) !== EMPTY_FIELD || squares[i] !== EMPTY_FIELD) {
+      return;
+    }
+    squares[i] = this.state.isPlayerOneNext ? PLAYER_ONE : PLAYER_TWO;
+    this.setState({
+      history: history.concat([{
+        squares
+      }]),
+      isPlayerOneNext: !this.state.isPlayerOneNext,
+    });
+  }
+
+  private calculateWinner(squares: Field[]): Field {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return EMPTY_FIELD;
   }
 }
 
