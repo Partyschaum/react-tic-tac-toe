cdbf1204244893b600a913d99f8bc84733039ad9
Hauke Stange <hauke.stange@jimdo.com>
2017-05-18 12:49:34 +0200

    Add increment / decrement handlers

--- src/components/Hello.test.tsx
+++ src/components/Hello.test.tsx
@@ -2,29 +2,54 @@ import * as React from 'react';
 import * as enzyme from 'enzyme';
 import Hello from './Hello';
 
-it('renders the correct text when no ehtusiasm level is given', () => {
-  const hello = enzyme.shallow(<Hello name="Katharina" />);
-  expect(hello.find('.greeting').text()).toEqual('Hello Katharina!');
+describe('greeting message rendering', () => {
+  it('renders the correct text when no ehtusiasm level is given', () => {
+    const hello = enzyme.shallow(<Hello name="Katharina" />);
+    expect(hello.find('.greeting').text()).toEqual('Hello Katharina!');
+  });
+
+  it('renders the correct text with an explicit enthusiasm level of 1', () => {
+    const hello = enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={1} />);
+    expect(hello.find('.greeting').text()).toEqual('Hello Katharina!');
+  });
+
+  it('renders the correct text with an explicit enthusiasm level of 5', () => {
+    const hello = enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={5} />);
+    expect(hello.find('.greeting').text()).toEqual('Hello Katharina!!!!!');
+  });
+
+  it('throws when the enthusiasm levle is 0', () => {
+    expect(() => {
+      enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={0} />);
+    }).toThrow();
+  });
+
+  it('throws when the enthusiasm level is negative', () => {
+    expect(() => {
+      enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={-1} />);
+    }).toThrow();
+  });
 });
 
-it('renders the correct text with an explicit enthusiasm level of 1', () => {
-  const hello = enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={1} />);
-  expect(hello.find('.greeting').text()).toEqual('Hello Katharina!');
-});
+describe('callback handling', () => {
+  const onIncrement = jest.fn();
+  const onDecrement = jest.fn();
 
-it('renders the correct text with an explicit enthusiasm level of 5', () => {
-  const hello = enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={5} />);
-  expect(hello.find('.greeting').text()).toEqual('Hello Katharina!!!!!');
-});
+  const component = enzyme.shallow(
+    <Hello
+      name="Katharina" enthusiasmLevel={1}
+      onIncrement={onIncrement}
+      onDecrement={onDecrement}
+    />
+  );
 
-it('throws when the enthusiasm levle is 0', () => {
-  expect(() => {
-    enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={0} />);
-  }).toThrow();
-});
+  it('handles onDecrement', () => {
+    component.find('button').at(0).simulate('click');
+    expect(onDecrement).toHaveBeenCalled();
+  });
 
-it('throws when the enthusiasm level is negative', () => {
-  expect(() => {
-    enzyme.shallow(<Hello name="Katharina" enthusiasmLevel={-1} />);
-  }).toThrow();
+  it('handles onIncrement', () => {
+    component.find('button').at(1).simulate('click');
+    expect(onIncrement).toHaveBeenCalled();
+  });
 });
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -5,17 +5,25 @@ import './Hello.css';
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
 
   return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMarks(enthusiasmLevel)}
+    <div>
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getExclamationMarks(enthusiasmLevel)}
+        </div>
+      </div>
+      <div>
+        <button id="hase" onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
       </div>
     </div>
   );
